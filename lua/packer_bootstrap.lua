cfd307192936ecb169c988d1dc3cedd99b0f8b95
fnurk <fnurkolito@protonmail.com>
2022-10-09 11:12:11 +0200

    adds chaotic WIP setup

---
+++ lua/packer_bootstrap.lua
@@ -0,0 +1,14 @@
+local packer_bootstrap = {}
+
+function packer_bootstrap.run()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+    		return true
+	end
+  	return false
+end
+
+return packer_bootstrap
